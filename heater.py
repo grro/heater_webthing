e5ade7e2d38f398249ef56b7b3b6faf868fc0644
grro <gregor.roth@1und1.de>
2024-05-02 19:23:41 +0200

    initial checkin

---
+++ heater.py
@@ -0,0 +1,295 @@
+import json
+from requests import Session
+from datetime import datetime, timedelta
+from typing import List, Optional
+from threading import Thread
+from redzoo.math.display import duration
+from time import sleep
+from string import Template
+from redzoo.database.simple import SimpleDB
+import logging
+
+
+SHELLY_SCRIPT_TEMPLATE = Template('''
+    Shelly.addStatusHandler(function(e) {
+     if (e.component === "switch:$id") {
+        if (e.delta.output === true) {
+          print("heater $id is on");
+          Timer.set(45*60*1000, false, function (ud) {
+                 Shelly.call("Switch.set", {'id': $id, 'on': false});
+              }, null);
+        } else {
+          print("heater $id is off");
+        }
+      }
+    });
+''')
+
+
+
+class Shelly3Pro:
+
+    def __init__(self, addr: str):
+        self.__session = Session()
+        self.addr = addr
+
+    def query(self, id: int) -> bool:
+        uri = self.addr + '/rpc/Switch.GetStatus?id=' + str(id)
+        try:
+            resp = self.__session.get(uri, timeout=10)
+            try:
+                data = resp.json()
+                return bool(data['output'])
+            except Exception as e:
+                raise Exception("called " + uri + " got " + str(resp.status_code) + " " + resp.text + " " + str(e))
+        except Exception as e:
+            self.__renew_session()
+            raise e
+
+    def switch(self, id: int, on: bool):
+        uri = self.addr + '/rpc/Switch.Set?id=' + str(id) + '&on=' + ('true' if on else 'false')
+        try:
+            resp = self.__session.get(uri, timeout=10)
+            if resp.status_code != 200:
+                raise Exception("called " + uri + " got " + str(resp.status_code) + " " + resp.text)
+        except Exception as e:
+            self.__renew_session()
+            raise Exception("called " + uri + " got " + str(e))
+
+    def upload_script(self, id: int, code: str):
+        uri = self.addr + '/rpc/Script.GetStatus?id=' + str(id)
+        resp = self.__session.get(uri)
+        script_exists = resp.status_code == 200
+        if script_exists:
+            uri = self.addr + '/rpc/Script.Stop?id=' + str(id)
+            resp  = self.__session.get(uri)
+            if resp.status_code == 200:
+                logging.debug("shelly script " + str(id) + " stopped " + resp.text)
+            else:
+                logging.warning("could not stop shelly script " + str(id) + " " + resp.text)
+        else:
+            uri = self.addr + '/rpc/Script.Create?'
+            req_data = json.dumps({"id": id, "name": "auto_off_" + str(id-1)}, ensure_ascii=False)
+            resp = self.__session.post(uri, data=req_data.encode("utf-8"), timeout=15)
+            if resp.status_code == 200:
+                logging.debug("shelly script " + str(id) + " created " + resp.text)
+            else:
+                logging.warning("could not create shelly script " + str(id) + " " + resp.text)
+
+            uri = self.addr + '/rpc/Script.PutCode'
+            req_data = json.dumps({"id": id, "code": code, "append": False}, ensure_ascii=False)
+            resp = self.__session.post(uri, data=req_data.encode("utf-8"), timeout=15)
+            if resp.status_code == 200:
+                logging.info("shelly script " + str(id) + " uploaded")
+            else:
+                logging.warning("could not upload shelly script " + str(id) + " " + resp.text)
+
+        self.enable_script(id)
+        self.restart_script(id)
+
+    def enable_script(self, id: int):
+        uri = self.addr + '/rpc/Script.SetConfig?id=' + str(id) + "&config={%22enable%22:true}"
+        resp = self.__session.get(uri, timeout=15)
+        if resp.status_code == 200:
+            logging.debug("shelly script " + str(id) + " enabled " + resp.text)
+        else:
+            logging.debug("could not enable shelly script " + str(id) + " " + resp.text)
+
+
+    def restart_script(self, id: int):
+        uri = self.addr + '/rpc/Script.GetStatus?id=' + str(id)
+        try:
+            resp = self.__session.get(uri)
+            if not resp.json()['running']:
+                resp  = self.__session.get(self.addr + '/rpc/Script.Start?id=' + str(id))
+                if resp.status_code == 200:
+                    logging.info("shelly script " + str(id) + " (re)started")
+                else:
+                    logging.debug("could not (re)start shelly script " + str(id) + " " + resp.text)
+        except Exception as e:
+            self.__renew_session()
+            logging.warning("called " + uri + " got " + str(e))
+
+    def __renew_session(self):
+        logging.info("renew session")
+        try:
+            self.__session.close()
+        except Exception as e:
+            logging.warning(str(e))
+        self.__session = Session()
+
+
+
+
+class HeatingRod:
+
+    def __init__(self, shelly: Shelly3Pro, id: int, directory: str):
+        self.__shelly = shelly
+        self.last_activation_time = datetime.now()
+        self.last_deactivation_time = datetime.now()
+        self.id = id
+        self.is_activated = False
+        self.__heating_secs_per_day = SimpleDB("heater_" + str(id), sync_period_sec=60, directory=directory)
+        self.deactivate()
+
+    def sync(self):
+        self.is_activated = self.__shelly.query(self.id)
+
+    def activate(self):
+        self.__shelly.switch(self.id, True)
+        self.is_activated = True
+        self.last_activation_time = datetime.now()
+        logging.debug(self.__str__() + " activated")
+
+    def deactivate(self):
+        if self.is_activated:
+            self.last_deactivation_time = datetime.now()
+            heating_time = (datetime.now() - self.last_activation_time)
+            day = datetime.now().strftime('%j')
+            self.__heating_secs_per_day.put(day, self.__heating_secs_per_day.get(day, 0) + heating_time.total_seconds())
+            self.__heating_secs_per_day.put((datetime.now() + timedelta(days=1)).strftime('%j'), 0)   # clean up next day value
+            logging.debug(self.__str__() + " deactivated (heating time " + duration(heating_time.total_seconds(), 1) + ")")
+        self.__shelly.switch(self.id, False)
+        self.is_activated = False
+
+    def heating_secs_of_day(self, day_of_year: int) -> Optional[int]:
+        secs = self.__heating_secs_per_day.get(str(day_of_year), -1)
+        if secs > 0:
+            return secs
+        else:
+            return None
+
+    def __str__(self):
+        return "heating rod " + str(self.id)
+
+
+
+class Heater:
+
+    def __init__(self, addr: str, directory: str):
+        self.__is_running = True
+        self.__listener = lambda: None    # "empty" listener
+        self.__shelly = Shelly3Pro(addr)
+        self.__heating_rods = [HeatingRod(self.__shelly, 0, directory), HeatingRod(self.__shelly, 1, directory), HeatingRod(self.__shelly, 2, directory)]
+        self.__last_time_decreased = datetime.now() - timedelta(minutes=10)
+        self.__last_time_increased = datetime.now() - timedelta(minutes=10)
+        self.__register_scripts()
+
+    def set_listener(self, listener):
+        self.__listener = listener
+
+    def heater_consumption(self, day_of_year: int) -> Optional[int]:
+        heater_secs_of_day = [heating_rod.heating_secs_of_day(day_of_year) for heating_rod in self.__heating_rods]
+        heater_secs_of_day = [secs for secs in heater_secs_of_day if secs is not None]
+        if len(heater_secs_of_day) > 0:
+            return int((sum(heater_secs_of_day) * self.power_step) / 60)
+        else:
+            return None
+
+    @property
+    def heater_consumption_today(self) -> int:
+        day_of_year = int(datetime.now().strftime('%j'))
+        consumption = self.heater_consumption(day_of_year)
+        if consumption is None:
+            consumption = 0
+        return consumption
+
+    @property
+    def __heater_consumption_list_current_year(self) -> List[int]:
+        current_day = int(datetime.now().strftime('%j'))
+        consumption_per_day = [self.heater_consumption(day_of_year) for day_of_year in range(0, current_day+1)]
+        return [consumption for consumption in consumption_per_day if consumption is not None]
+
+    @property
+    def heater_consumption_current_year(self) -> int:
+        return sum(self.__heater_consumption_list_current_year)
+
+    @property
+    def heater_consumption_estimated_year(self) -> int:
+        consumptions_per_day = self.__heater_consumption_list_current_year
+        if len(consumptions_per_day) > 0:
+            return int(sum(consumptions_per_day) * 365 / len(consumptions_per_day))
+        else:
+            return 0
+
+    @property
+    def num_heating_rods_active(self) -> int:
+        return len([heating_rod for heating_rod in self.__heating_rods if heating_rod.is_activated])
+
+    @property
+    def __sorted_heating_rods(self) -> List[HeatingRod]:
+        heating_rod_list = list(self.__heating_rods)
+        for i in range(0, datetime.now().day):
+            heater = heating_rod_list.pop(0)
+            heating_rod_list.append(heater)
+        return heating_rod_list
+
+    def get_heating_rod(self, id: int) -> HeatingRod:
+        for heater in self.__heating_rods:
+            if heater.id == id:
+                return heater
+        return None
+
+    @property
+    def power_step(self) -> int:
+        return 520
+
+    @property
+    def power(self) -> int:
+        return self.num_heating_rods_active * self.power_step
+
+    def set_power(self, power: int):
+        if self.power > power:
+            self.decrease()
+        elif self.power < power:
+            self.increase()
+
+    def increase(self):
+        if datetime.now() > (self.__last_time_increased + timedelta(minutes=1 + self.num_heating_rods_active)):
+            self.__last_time_increased = datetime.now()
+            for heating_rod in [heating_rod for heating_rod in self.__sorted_heating_rods if not heating_rod.is_activated]:
+                heating_rod.activate()          # increase heater power (1 heater only)
+                break
+            self.__measure()
+
+    def decrease(self):
+        if datetime.now() > (self.__last_time_decreased + timedelta(seconds=10)):
+            self.__last_time_decreased = datetime.now()
+            for heating_rod in [heating_rod for heating_rod in self.__sorted_heating_rods if heating_rod.is_activated]:
+                heating_rod.deactivate()        # decrease heater power consumption (1 heater only)
+                break
+            self.__measure()
+
+    def stop(self):
+        self.__is_running = False
+
+    def start(self):
+        Thread(target=self.__measure, daemon=True).start()
+        Thread(target=self.__auto_decrease, daemon=True).start()
+        Thread(target=self.__auto_restart_scripts, daemon=True).start()
+
+    def __measure(self):
+        while self.__is_running:
+            for heating_rods in self.__heating_rods:
+                heating_rods.sync()
+            self.__listener()
+            sleep(15)
+
+    def __auto_decrease(self):
+        while self.__is_running:
+            auto_decrease_time_min = 17
+            if datetime.now() > (self.__last_time_decreased + timedelta(minutes=auto_decrease_time_min)) or datetime.now() > (self.__last_time_increased + timedelta(minutes=auto_decrease_time_min)):
+                logging.info("auto decrease (" + str(auto_decrease_time_min) + " min)")
+                self.decrease()
+            sleep(60)
+
+    def __auto_restart_scripts(self):
+        while self.__is_running:
+            for id in range (0, 3):
+                self.__shelly.restart_script(id+1)
+            sleep(7 * 60 * 60)
+
+    def __register_scripts(self):
+        for id in range (0, 3):
+            self.__shelly.upload_script(id+1, SHELLY_SCRIPT_TEMPLATE.substitute({"id": id}))
+
---
+++ heater_webthing.py
@@ -0,0 +1,170 @@
+from webthing import (SingleThing, Property, Thing, Value, WebThingServer)
+import sys
+import logging
+import tornado.ioloop
+from heater import Heater
+
+
+
+
+class HeaterThing(Thing):
+
+    # regarding capabilities refer https://iot.mozilla.org/schemas
+    # there is also another schema registry http://iotschema.org/docs/full.html not used by webthing
+
+    def __init__(self, description: str, heater: Heater):
+        Thing.__init__(
+            self,
+            'urn:dev:ops:heater-1',
+            'HeaterSwitch',
+            ['MultiLevelSensor'],
+            description
+        )
+        self.ioloop = tornado.ioloop.IOLoop.current()
+        self.heater = heater
+        self.heater.set_listener(self.on_value_changed)
+
+        self.power = Value(heater.power, heater.set_power)
+        self.add_property(
+            Property(self,
+                     'power',
+                     self.power,
+                     metadata={
+                         'title': 'power',
+                         "type": "integer",
+                         'description': 'the heater power (watt)',
+                         'readOnly': False,
+                     }))
+
+        self.power_step = Value(heater.power_step)
+        self.add_property(
+            Property(self,
+                     'power_step',
+                     self.power_step,
+                     metadata={
+                         'title': 'power_step',
+                         "type": "integer",
+                         'description': 'the heater power step (watt) ',
+                         'readOnly': True,
+                     }))
+
+        self.num_heating_rods_active = Value(heater.num_heating_rods_active)
+        self.add_property(
+            Property(self,
+                     'num_heating_rods_active',
+                     self.num_heating_rods_active,
+                     metadata={
+                         'title': 'num_heating_rods_active',
+                         "type": "integer",
+                         'description': 'num heating rods active',
+                         'readOnly': True,
+                     }))
+
+
+        self.num_heating_rod0_activated = Value(heater.get_heating_rod(0))
+        self.add_property(
+            Property(self,
+                     'num_heating_rod0_activated',
+                     self.num_heating_rod0_activated,
+                     metadata={
+                         'title': 'num_heating_rod0_activated',
+                         "type": "boolean",
+                         'description': 'true, if heating rod 0 is activated',
+                         'readOnly': True,
+                     }))
+
+        self.num_heating_rod1_activated = Value(heater.get_heating_rod(1))
+        self.add_property(
+            Property(self,
+                     'num_heating_rod1_activated',
+                     self.num_heating_rod1_activated,
+                     metadata={
+                         'title': 'num_heating_rod1_activated',
+                         "type": "boolean",
+                         'description': 'true, if heating rod 1 is activated',
+                         'readOnly': True,
+                     }))
+
+        self.num_heating_rod2_activated = Value(heater.get_heating_rod(2))
+        self.add_property(
+            Property(self,
+                     'num_heating_rod2_activated',
+                     self.num_heating_rod2_activated,
+                     metadata={
+                         'title': 'num_heating_rod2_activated',
+                         "type": "boolean",
+                         'description': 'true, if heating rod 2 is activated',
+                         'readOnly': True,
+                     }))
+
+        self.heater_consumption_today = Value(heater.heater_consumption_today)
+        self.add_property(
+            Property(self,
+                     'heater_consumption_today',
+                     self.heater_consumption_today,
+                     metadata={
+                         'title': 'heater_consumption_today',
+                         "type": "integer",
+                         'description': 'heater power consumption current day',
+                         'readOnly': True,
+                     }))
+
+        self.heater_consumption_current_year = Value(heater.heater_consumption_current_year)
+        self.add_property(
+            Property(self,
+                     'heater_consumption_current_year',
+                     self.heater_consumption_current_year,
+                     metadata={
+                         'title': 'heater_consumption_current_year',
+                         "type": "integer",
+                         'description': 'heater power consumption current year',
+                         'readOnly': True,
+                     }))
+
+        self.heater_consumption_estimated_year = Value(heater.heater_consumption_estimated_year)
+        self.add_property(
+            Property(self,
+                     'heater_consumption_estimated_year',
+                     self.heater_consumption_estimated_year,
+                     metadata={
+                         'title': 'heater_consumption_estimated_year',
+                         "type": "integer",
+                         'description': 'heater power consumption current year estimated',
+                         'readOnly': True,
+                     }))
+
+    def on_value_changed(self):
+        self.ioloop.add_callback(self._on_value_changed)
+
+    def _on_value_changed(self):
+        self.power.notify_of_external_update(self.heater.power)
+        self.power_step.notify_of_external_update(self.heater.power_step)
+        self.num_heating_rods_active.notify_of_external_update(self.heater.num_heating_rods_active)
+        self.num_heating_rod0_activated.notify_of_external_update(self.heater.get_heating_rod(0).is_activated)
+        self.num_heating_rod1_activated.notify_of_external_update(self.heater.get_heating_rod(1).is_activated)
+        self.num_heating_rod2_activated.notify_of_external_update(self.heater.get_heating_rod(2).is_activated)
+        self.heater_consumption_today.notify_of_external_update(self.heater.heater_consumption_today)
+        self.heater_consumption_today.notify_of_external_update(self.heater.heater_consumption_today)
+        self.heater_consumption_current_year.notify_of_external_update(self.heater.heater_consumption_current_year)
+        self.heater_consumption_estimated_year.notify_of_external_update(self.heater.heater_consumption_estimated_year)
+
+
+def run_server(description: str, port: int, addr: str, directory: str):
+    heater = Heater(addr, directory)
+    server = WebThingServer(SingleThing(HeaterThing(description, heater)), port=port, disable_host_validation=True)
+    try:
+        logging.info('starting the server http://localhost:' + str(port) + " (addr=" + addr + ")")
+        heater.start()
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('stopping the server')
+        heater.stop()
+        server.stop()
+        logging.info('done')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(name)-20s: %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.getLogger('tornado.access').setLevel(logging.ERROR)
+    logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
+    run_server("description", int(sys.argv[1]), sys.argv[2], sys.argv[3])
