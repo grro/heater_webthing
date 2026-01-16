d3ef74190920387f398449be547043fd44ad1c4d
grro <gregror.roth@1und1.de>
2026-01-16 17:21:03 +0100

    fix mcp server

--- heater_mcp.py
+++ heater_mcp.py
@@ -9,17 +9,18 @@ class HeaterMCPServer(MCPServer):
         super().__init__("heater", port)
         self.heater = heater
 
-        @self.mcp.resource("resource://power", description="Current energy consumption in Watt")
-        def get_power() -> int:
-            return heater.power
+        @self.mcp.tool(name="get_heater_power", description="Current heater energy consumption in Watt")
+        def get_heater_power() -> int:
+            return self.heater.power
 
-        @self.mcp.resource("resource://heating_rods_active", description="Number of active heating rods")
-        def get_heating_rods_active() -> int:
-            return heater.heating_rods_active
+        @self.mcp.tool(name="get_active_heating_rods", description="Number of currently active heating rods")
+        def get_active_heating_rods() -> int:
+            return self.heater.heating_rods_active
 
-        @self.mcp.tool("resource://heating_rods_activation", description="set the number of active heating rods")
-        def heating_rods_activation(new_num: int) -> None:
+        @self.mcp.tool(name="set_active_heating_rods", description="Set the number of active heating rods (control the heater)")
+        def set_active_heating_rods(new_num: int) -> str:
             self.heater.set_heating_rods_active(new_num)
+            return f"Active heating rods set to {new_num}"
 
 
 # npx @modelcontextprotocol/inspector
